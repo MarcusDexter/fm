1adeceeaaaa404929a56d700d74d94838eff8509
MarcusDexter <a@cys.pw>
2015-08-28 00:28:08 +0800

    实现多文件上传化学
    添加注释，优化代码，取消了本不应该存在后台向前台传递path的操作

--- server.php
+++ server.php
@@ -6,10 +6,13 @@ require_once 'file.func.php';
 require_once 'common.func.php';
 require_once 'upload.class.php';
 
-$path="file";
-$path=$_REQUEST['path']?$_REQUEST['path']:$path;
+// 获取路径
+$path=$_REQUEST['path']?$_REQUEST['path']:"file";
+// 获取操作
 $act=$_REQUEST['act'];
+// 获取文件名
 $filename=$_REQUEST['filename'];
+// 获取文件夹名
 $dirname=$_REQUEST['dirname'];
 
 $msg = "";
@@ -27,12 +30,16 @@ if ($act=="delFolder") {
 }elseif($act=="createFile"){
 	$msg = createFile($path.'/'.$filename);
 }elseif($_FILES){
-	$upload=new upload('file0',$path);
-	$msg=$upload->uploadFile();
+	for ($i=0; $i < count($_FILES) ; $i++) {
+		$upload=new upload("file".$i,$path);
+		$msg.=$upload->uploadFile();
+	}
 }
 
+// 获取文件夹内文件夹及文件信息
 $info=readDirectory($path);
 
+// 文件夹信息格式化并用数组保存
 $folderInfo = array();
 for ($i=0; $i < count($info[dir]); $i++) { 
 	array_push($folderInfo, $info[dir][$i]);
@@ -40,16 +47,17 @@ for ($i=0; $i < count($info[dir]); $i++) {
 	array_push($folderInfo,date("Y-m-d H:i:s",filemtime($path.'/'.$info[dir][$i])));
 }
 
+// 文件信息格式化并用数组保存
 $fileInfo = array();
 for ($i=0; $i < count($info[file]); $i++) { 
 	array_push($fileInfo, $info[file][$i]);
 	array_push($fileInfo,transByte(fileSize($path.'/'.$info[file][$i])));
 	array_push($fileInfo,date("Y-m-d H:i:s",filemtime($path.'/'.$info[file][$i])));
 }
+
+// 数组打包
 $tableInfo = array('folderInfo' => $folderInfo, 'fileInfo' => $fileInfo);
-
-
-$arr = array('tableInfo' => $tableInfo, 'path' => $path, 'msg' => $msg);
+$arr = array('tableInfo' => $tableInfo, 'msg' => $msg);
 
 echo json_encode($arr);
 return ;
